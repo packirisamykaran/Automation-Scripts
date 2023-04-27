7bc9ef10d3b689ff910ec0bb8e7d1fe84dcaed40
Karan <packirisamykaran@gmail.com>
2023-04-27 20:54:16 +0800

    Update grant.py

--- grant scraper/grant.py
+++ grant scraper/grant.py
@@ -1,10 +1,51 @@
-import requests
-from bs4 import BeautifulSoup
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.service import Service as ChromeService
+from webdriver_manager.chrome import ChromeDriverManager
 
-URL = "https://www.blockchaingrants.org/"
-page = requests.get(URL)
+# instantiate options
+options = webdriver.ChromeOptions()
 
-soup = BeautifulSoup(page.content, "html.parser")
+# run browser in headless mode
+# options.headless = True
+
+# instantiate driver
+driver = webdriver.Chrome(service=ChromeService(
+    ChromeDriverManager().install()), options=options)
+
+# load website
+url = 'https://www.blockchaingrants.org/'
+
+# get the entire website content
+driver.get(url)
 
 
-print(page.text)
+# select elements by class name
+elements = driver.find_elements(By.CLASS_NAME, 'mYVXT')
+
+# container = elements.find_elements(By.CLASS_NAME, "LS81yb")
+
+
+for element in elements:
+    a = element.find_elements(By.CLASS_NAME, 'JNdkSc-SmKAyb')
+
+    for title in a:
+        # select H2s, within element, by tag name
+        heading = title.find_element(By.CLASS_NAME, 'C9DxTc').text
+        # print H2s
+        print(heading)
+
+
+# # load website
+# url = 'https://angular.io/'
+
+# # get the entire website content
+# driver.get(url)
+
+# # select elements by class name
+# elements = driver.find_elements(By.CLASS_NAME, 'text-container')
+# for title in elements:
+#     # select H2s, within element, by tag name
+#     heading = title.find_element(By.TAG_NAME, 'h2').text
+#     # print H2s
+#     print(title)
